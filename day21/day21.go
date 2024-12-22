b80995b2e55a3630ced3241e7eb3d66c49b4779b
Matthew Bourke <matthew.james.bourke@gmail.com>
2024-12-23 01:04:19 +1100

    day 21 part 1

--- day21/day21.go
+++ day21/day21.go
@@ -36,64 +36,14 @@ var (
 	UP_VAL    = 3
 )
 
-func getDirPadNeighbours(n int) []int {
-	switch n {
-	case 0:
-		return []int{1}
-	case 1:
-		return []int{0, 2, 3}
-	case 2:
-		return []int{1, 10}
-	case 3:
-		return []int{2, 10}
-	case 10:
-		return []int{2, 3}
-	}
-
-	return []int{}
-}
-
-// type Keypad = [][]int // gap = -1, A = 10, < = 0, v = 1, > = 2, ^ = 3
-
-type Keypad = utils.Grid[int]
-
 type Pos struct {
 	x int
 	y int
 }
 
 type Robot struct {
-	name       string
-	keypad     Keypad
 	pos        Pos
 	controller *Robot
-}
-
-func (r Robot) String() string {
-	return fmt.Sprintf("%s{Pos: (%d, %d), Keypad:\n%v}", r.name, r.pos.x, r.pos.y, r.keypad)
-}
-
-type State struct {
-	numPadPos Pos
-	keypad1   Pos
-	keypad2   Pos
-	dir       Pos
-}
-
-type ScoredState struct {
-	state   State
-	moveSet string
-}
-
-type DirPadState struct {
-	keypad1 Pos
-	keypad2 Pos
-	dir     Pos
-}
-
-type ScoredDirPadState struct {
-	dirPadState DirPadState
-	moveSet     string
 }
 
 func Run() {
@@ -114,346 +64,235 @@ func Run() {
 }
 
 func part1(lines []string) int {
-	numericKeypad := Keypad{
-		{7, 8, 9},
-		{4, 5, 6},
-		{1, 2, 3},
-		{-1, 0, 10},
-	}
-
-	directionalKeypad1 := Keypad{
-		{-1, 3, 10},
-		{0, 1, 2},
-	}
-	directionalKeypad2 := Keypad{
-		{-1, 3, 10},
-		{0, 1, 2},
-	}
-
-	moves := make(map[State]string)
-
-	// solution space is small enough to write them out
-
-	//test := dirRobot2.moveToAndPress(2, 1)
-	//test := doorRobot.moveToAndPress(ZERO.x, ZERO.y)
-	//fmt.Println(test)
-	//test = doorRobot.moveToAndPress(TWO.x, TWO.y)
-	//fmt.Println(test)
-
-	total := 0
-	lines = []string{"0"}
-	for _, line := range lines {
-		dirRobot1 := Robot{
-			name:   "DirRobot1",
-			keypad: directionalKeypad1,
-			pos:    A_DIR,
-		}
-
-		dirRobot2 := Robot{
-			name:       "DirRobot2",
-			keypad:     directionalKeypad2,
-			pos:        A_DIR,
-			controller: &dirRobot1,
-		}
-
-		doorRobot := Robot{
-			name:       "DoorRobot",
-			keypad:     numericKeypad,
-			pos:        A_NUM,
-			controller: &dirRobot2,
-		}
-
-		numeric, _ := strconv.Atoi(line[:len(line)-1])
-
-		startState := State{numPadPos: A_NUM, keypad1: Pos{x: 2, y: 0}, keypad2: Pos{x: 2, y: 0}}
-		numPadQueue := make(chan State, 11*5*5*4)
-		numPadQueue <- startState
-
-		finalStates := make([]ScoredState, 0)
-		var inputSequence string
-		for _, c := range line {
-			coord := getCoord(c)
-			for len(numPadQueue) > 0 {
-				current := <-numPadQueue
-				neighbours := getNumPadNeighbourStates(numericKeypad, current, doorRobot, coord)
-
-				for _, neighbour := range neighbours {
-					if oldMoves, ok := moves[neighbour.state]; ok {
-						if len(neighbour.moveSet) < len(oldMoves) {
-							moves[neighbour.state] = neighbour.moveSet
-							numPadQueue <- neighbour.state
-						}
-					} else {
-						moves[neighbour.state] = neighbour.moveSet
-						numPadQueue <- neighbour.state
-					}
-
-					if neighbour.state.numPadPos == coord {
-						finalStates = append(finalStates, neighbour)
-					}
-				}
-			}
-
-			fmt.Println("NUMPAD Final States:")
-			fmt.Println(finalStates)
-			//inputSequence += moveToAndPress(doorRobot, coord.x, coord.y)
-			//fmt.Println(inputSequence)
-			//inputSequence += dirRobot1.sendToController()
-		}
-		fmt.Printf("%s: Len = %d Value = %d\n", inputSequence, len(inputSequence), numeric)
-		total += numeric * len(inputSequence)
-	}
-
-	fmt.Printf("\n\nTest Area:\n")
+	// notable property that after each iteration, robots will all be returned to starting positions
 	dirRobot1 := Robot{
-		name:   "DirRobot1",
-		keypad: directionalKeypad1,
-		pos:    A_DIR,
+		pos: A_DIR,
 	}
+
 	dirRobot2 := Robot{
-		name:       "DirRobot2",
-		keypad:     directionalKeypad2,
 		pos:        A_DIR,
 		controller: &dirRobot1,
 	}
+
 	doorRobot := Robot{
-		name:       "DoorRobot",
-		keypad:     numericKeypad,
 		pos:        A_NUM,
 		controller: &dirRobot2,
 	}
-	fmt.Println(doorRobot)
-	//currentState := DirPadState{keypad1: Pos{x: 0, y: 1}}
-	currentState := State{keypad1: A_DIR, keypad2: A_DIR}
-	upResult := getControllerMoveset2(UP, currentState, &dirRobot2)
-	upState := currentState
-	upState.keypad1 = upResult[0].dirPadState.keypad1
-	upState.keypad2 = upResult[0].dirPadState.keypad2
-	fmt.Println("UP RESULT")
-	fmt.Println(upResult)
 
-	leftState := currentState
-	leftResult := getControllerMoveset2(LEFT, currentState, &dirRobot2)
-	leftState.keypad1 = leftResult[0].dirPadState.keypad1
-	leftState.keypad2 = leftResult[0].dirPadState.keypad2
-	fmt.Println("LEFT RESULT")
-	fmt.Println(leftResult)
+	total := 0
+	for _, line := range lines {
+		var moveset string
+		numeric, _ := strconv.Atoi(line[:len(line)-1])
 
-	/*
-		for _, r := range result {
-			//newState := State{keypad1: r.dirPadState.keypad1, keypad2: r.dirPadState.keypad2}
-			backToA := getControllerMoveset1(A_DIR, r.dirPadState, &dirRobot1)
-			ways.Insert(r.moveSet + backToA + "A")
-		}
-	*/
+		// for each number, figure out moves to get there
+		for _, x := range line {
+			c := getCoord(x)
+			xDiff := c.x - doorRobot.pos.x
+			yDiff := c.y - doorRobot.pos.y
 
-	upLeftResult := getControllerMoveset2(LEFT, upState, &dirRobot2)
-	leftUpResult := getControllerMoveset2(UP, leftState, &dirRobot2)
+			horizontalMoves := make([]Pos, 0)
+			verticalMoves := make([]Pos, 0)
 
-	upLeft := upResult[0].moveSet + upLeftResult[0].moveSet
-	fmt.Println(upLeft)
-
-	leftUp := leftResult[0].moveSet + leftUpResult[0].moveSet
-	fmt.Println(leftUp)
-
-	return total
-	// 160800 too high
-	// 125767 too low
-}
-
-func getNumPadNeighbourStates(numPad Keypad, current State, numPadRobot Robot, desiredPos Pos) []ScoredState {
-	neighbours := make([]ScoredState, 0)
-
-	// UP
-	if current.numPadPos.y > 0 && current.numPadPos.y > desiredPos.y {
-		upMoves := getControllerMoveset2(UP, current, numPadRobot.controller)
-		for _, move := range upMoves {
-			nextPos := Pos{x: current.numPadPos.x, y: current.numPadPos.y - 1}
-			nextState := State{numPadPos: nextPos, keypad2: move.dirPadState.keypad2, keypad1: move.dirPadState.keypad1}
-			upScoredState := ScoredState{moveSet: move.moveSet, state: nextState}
-			neighbours = append(neighbours, upScoredState) //numPad[current.numPadPos.y-1][current.numPadPos.x])
-		}
-	}
-
-	// DOWN
-	if current.numPadPos.y < len(numPad)-1 && numPad[current.numPadPos.y+1][current.numPadPos.x] != -1 &&
-		current.numPadPos.y < desiredPos.y {
-		downMoves := getControllerMoveset2(UP, current, numPadRobot.controller)
-		for _, move := range downMoves {
-			nextPos := Pos{x: current.numPadPos.x, y: current.numPadPos.y + 1}
-			nextState := State{numPadPos: nextPos, keypad2: move.dirPadState.keypad2, keypad1: move.dirPadState.keypad1}
-			downScoredState := ScoredState{moveSet: move.moveSet, state: nextState}
-			neighbours = append(neighbours, downScoredState) //numPad[current.numPadPos.y+1][current.numPadPos.x])
-
-		}
-	}
-
-	// LEFT
-	if current.numPadPos.x > 0 && numPad[current.numPadPos.y][current.numPadPos.x-1] != -1 &&
-		current.numPadPos.x > desiredPos.x {
-		leftMoves := getControllerMoveset2(UP, current, numPadRobot.controller)
-		for _, move := range leftMoves {
-			nextPos := Pos{x: current.numPadPos.x - 1, y: current.numPadPos.y}
-			nextState := State{numPadPos: nextPos, keypad2: move.dirPadState.keypad2, keypad1: move.dirPadState.keypad1}
-			leftScoredState := ScoredState{moveSet: move.moveSet, state: nextState}
-			neighbours = append(neighbours, leftScoredState) //numPad[current.numPadPos.y][current.numPadPos.x-1])
-		}
-	}
-
-	// RIGHT
-	if current.numPadPos.x < len(numPad[0])-1 && current.numPadPos.x < desiredPos.x {
-		rightMoves := getControllerMoveset2(UP, current, numPadRobot.controller)
-		for _, move := range rightMoves {
-			nextPos := Pos{x: current.numPadPos.x + 1, y: current.numPadPos.y}
-			nextState := State{numPadPos: nextPos, keypad2: move.dirPadState.keypad2, keypad1: move.dirPadState.keypad1}
-			rightScoredState := ScoredState{moveSet: move.moveSet, state: nextState}
-			neighbours = append(neighbours, rightScoredState) //numPad[current.numPadPos.y][current.numPadPos.x+1])
-		}
-	}
-
-	return neighbours
-}
-
-func getControllerMoveset2(desiredPosition Pos, current State, robot *Robot) []ScoredDirPadState {
-	if robot == nil {
-		fmt.Println("calculate raw input here")
-		return []ScoredDirPadState{}
-		// return topLevelMove(current.keypad1, desiredPosition)
-	}
-
-	if current.keypad2 == desiredPosition {
-		s := DirPadState{keypad1: current.keypad1, keypad2: current.keypad2, dir: current.dir}
-		return []ScoredDirPadState{{s, ""}}
-	}
-
-	moves := make(map[DirPadState]string)
-	//moveSet := ""
-	// now pathfind on the dirPad
-
-	queue := make(chan DirPadState, 5*5*4)
-	startState := DirPadState{keypad2: current.keypad2, keypad1: current.keypad1, dir: UP}
-	queue <- startState
-
-	finalStates := make([]ScoredDirPadState, 0)
-
-	for len(queue) > 0 {
-		currentDpad := <-queue
-
-		neighbours := getDpadNeighbours(robot, currentDpad, desiredPosition)
-		for _, neighbour := range neighbours {
-			if oldMoves, ok := moves[neighbour.dirPadState]; ok {
-				if len(neighbour.moveSet) < len(oldMoves) {
-					moves[neighbour.dirPadState] = neighbour.moveSet
-					queue <- neighbour.dirPadState
+			if xDiff < 0 {
+				for range -xDiff {
+					horizontalMoves = append(horizontalMoves, LEFT)
 				}
 			} else {
-				moves[neighbour.dirPadState] = neighbour.moveSet
-				queue <- neighbour.dirPadState
+				for range xDiff {
+					horizontalMoves = append(horizontalMoves, RIGHT)
+				}
 			}
 
-			if neighbour.dirPadState.keypad2 == desiredPosition {
-				finalStates = append(finalStates, neighbour)
+			if yDiff < 0 {
+				for range -yDiff {
+					verticalMoves = append(verticalMoves, UP)
+				}
+			} else {
+				for range yDiff {
+					verticalMoves = append(verticalMoves, DOWN)
+				}
 			}
-		}
-	}
 
-	for i, final := range finalStates {
-		backToA := getControllerMoveset1(A_DIR, final.dirPadState, robot.controller)
-		fmt.Println(backToA)
-		final.moveSet += backToA
-		finalStates[i] = final
-	}
+			if xDiff != 0 && yDiff != 0 {
+				// need to compare vertical first and horizontal first
+				// but also need to check if either will run into the gap
+				horizontalFirstController := &Robot{}
+				verticalFirstController := &Robot{}
+				*horizontalFirstController = *doorRobot.controller
+				*verticalFirstController = *doorRobot.controller
 
-	return finalStates
-}
+				var horizontalFirstMoveset string
+				horizontalHitsGap := doorRobot.pos.y == GAP_NUM.y && doorRobot.pos.x+xDiff == GAP_NUM.x
+				if !horizontalHitsGap {
+					hThenV := make([]Pos, 0)
+					hThenV = append(hThenV, horizontalMoves...)
+					hThenV = append(hThenV, verticalMoves...)
+					horizontalFirstMoveset = generateMoveset(hThenV, horizontalFirstController)
+				}
 
-func getDpadNeighbours(robot *Robot, dpadState DirPadState, desiredPos Pos) []ScoredDirPadState {
-	neighbours := make([]ScoredDirPadState, 0)
+				var verticalFirstMoveset string
+				verticalHitsGap := doorRobot.pos.x == GAP_NUM.x && doorRobot.pos.y+yDiff == GAP_NUM.y
+				if !verticalHitsGap {
+					vThenH := make([]Pos, 0)
+					vThenH = append(vThenH, verticalMoves...)
+					vThenH = append(vThenH, horizontalMoves...)
+					verticalFirstMoveset = generateMoveset(vThenH, verticalFirstController)
+				}
 
-	// UP
-	if dpadState.keypad2.y > 0 && desiredPos.y > dpadState.keypad2.y {
-		upMoves := getControllerMoveset1(UP, dpadState, robot.controller)
-		nextState := DirPadState{keypad1: UP, keypad2: Pos{x: dpadState.keypad2.x, y: dpadState.keypad2.y - 1}}
-		upScoredState := ScoredDirPadState{moveSet: upMoves, dirPadState: nextState}
-		neighbours = append(neighbours, upScoredState) //numPad[current.numPadPos.y-1][current.numPadPos.x])
-	}
-
-	// DOWN
-	if dpadState.keypad2.y < len(robot.keypad)-1 && robot.keypad[dpadState.keypad2.y+1][dpadState.keypad2.x] != -1 &&
-		desiredPos.y > dpadState.keypad2.y {
-		downMoves := getControllerMoveset1(DOWN, dpadState, robot.controller)
-		nextState := DirPadState{keypad1: DOWN, keypad2: Pos{x: dpadState.keypad2.x, y: dpadState.keypad2.y + 1}}
-		downScoredState := ScoredDirPadState{moveSet: downMoves, dirPadState: nextState}
-		neighbours = append(neighbours, downScoredState) //numPad[current.numPadPos.y+1][current.numPadPos.x])
-	}
-
-	// LEFT
-	if dpadState.keypad2.x > 0 && robot.keypad[dpadState.keypad2.y][dpadState.keypad2.x-1] != -1 &&
-		dpadState.keypad2.x > desiredPos.x {
-		leftMoves := getControllerMoveset1(LEFT, dpadState, robot.controller)
-		nextState := DirPadState{keypad1: LEFT, keypad2: Pos{x: dpadState.keypad2.x - 1, y: dpadState.keypad2.y}}
-		leftScoredState := ScoredDirPadState{moveSet: leftMoves, dirPadState: nextState}
-		neighbours = append(neighbours, leftScoredState) //numPad[current.numPadPos.y][current.numPadPos.x-1])
-	}
-
-	// RIGHT
-	if dpadState.keypad2.x < len(robot.keypad[0])-1 && dpadState.keypad2.x < desiredPos.x {
-		rightMoves := getControllerMoveset1(RIGHT, dpadState, robot.controller)
-		nextState := DirPadState{keypad1: RIGHT, keypad2: Pos{x: dpadState.keypad2.x + 1, y: dpadState.keypad2.y}}
-		rightScoredState := ScoredDirPadState{moveSet: rightMoves, dirPadState: nextState}
-		neighbours = append(neighbours, rightScoredState) //numPad[current.numPadPos.y][current.numPadPos.x+1])
-	}
-
-	return neighbours
-}
-
-func getControllerMoveset1(desiredPosition Pos, current DirPadState, robot *Robot) string {
-	// rawdog this one??
-	if robot.controller != nil {
-		panic("somtin gon areye ere")
-	}
-
-	var inputSequence string
-
-	currentPos := current.keypad1
-	xDiff := desiredPosition.x - currentPos.x
-	yDiff := desiredPosition.y - currentPos.y
-
-	for xDiff != 0 || yDiff != 0 {
-		if xDiff > 0 {
-			for xDiff != 0 {
-				inputSequence += ">"
-				xDiff--
+				// set moveset accordingly
+				if horizontalHitsGap {
+					moveset += verticalFirstMoveset
+					doorRobot.controller = verticalFirstController
+				} else if verticalHitsGap {
+					moveset += horizontalFirstMoveset
+					doorRobot.controller = horizontalFirstController
+				} else {
+					if len(horizontalFirstMoveset) <= len(verticalFirstMoveset) {
+						moveset += horizontalFirstMoveset
+						doorRobot.controller = horizontalFirstController
+					} else {
+						moveset += verticalFirstMoveset
+						doorRobot.controller = verticalFirstController
+					}
+				}
+			} else if xDiff != 0 {
+				// just go horizontal
+				moveset += generateMoveset(horizontalMoves, doorRobot.controller)
+			} else if yDiff != 0 {
+				// just go vertical
+				moveset += generateMoveset(verticalMoves, doorRobot.controller)
 			}
+
+			moveset += generateMoveset([]Pos{A_DIR}, doorRobot.controller)
+			doorRobot.pos = c
 		}
 
-		if yDiff > 0 {
-			for yDiff != 0 {
-				inputSequence += "v"
-				yDiff--
-			}
-		}
-
-		if xDiff < 0 {
-			for xDiff != 0 {
-				inputSequence += "<"
-				xDiff++
-			}
-		}
-
-		if yDiff < 0 {
-			for yDiff != 0 {
-				inputSequence += "^"
-				yDiff++
-			}
-		}
+		fmt.Printf("MOVES FOR %s: %s (Len = %d, Num = %d)\n", line, moveset, len(moveset), numeric)
+		total += numeric * len(moveset)
 	}
 
-	return inputSequence + "A"
+	return total
 }
 
 func part2(lines []string) int {
 	return 0
+}
+
+// Given a set of moves requested by next robot, figure out best moves to do those actions
+func generateMoveset(desiredMoves []Pos, robot *Robot) string {
+	if robot == nil {
+		return getHumanInput(desiredMoves)
+	}
+
+	var moveset string
+
+	for _, move := range desiredMoves {
+		xDiff := move.x - robot.pos.x
+		yDiff := move.y - robot.pos.y
+		horizontalMoves := make([]Pos, 0)
+		verticalMoves := make([]Pos, 0)
+
+		if xDiff < 0 {
+			for range -xDiff {
+				horizontalMoves = append(horizontalMoves, LEFT)
+			}
+		} else {
+			for range xDiff {
+				horizontalMoves = append(horizontalMoves, RIGHT)
+			}
+		}
+
+		if yDiff < 0 {
+			for range -yDiff {
+				verticalMoves = append(verticalMoves, UP)
+			}
+		} else {
+			for range yDiff {
+				verticalMoves = append(verticalMoves, DOWN)
+			}
+		}
+
+		if xDiff != 0 && yDiff != 0 {
+			// need to compare vertical first and horizontal first
+			// but also need to check if either will run into the gap
+			horizontalFirstController := &Robot{}
+			verticalFirstController := &Robot{}
+			if robot.controller != nil {
+				*horizontalFirstController = *robot.controller
+				*verticalFirstController = *robot.controller
+			} else {
+				horizontalFirstController = nil
+				verticalFirstController = nil
+			}
+
+			var horizontalFirstMoveset string
+			horizontalHitsGap := robot.pos.y == GAP_DIR.y && robot.pos.x+xDiff == GAP_DIR.x
+			if !horizontalHitsGap {
+				hThenV := make([]Pos, 0)
+				hThenV = append(hThenV, horizontalMoves...)
+				hThenV = append(hThenV, verticalMoves...)
+				horizontalFirstMoveset = generateMoveset(hThenV, horizontalFirstController)
+			}
+
+			var verticalFirstMoveset string
+			verticalHitsGap := robot.pos.x == GAP_DIR.x && robot.pos.y+yDiff == GAP_DIR.y
+			if !verticalHitsGap {
+				vThenH := make([]Pos, 0)
+				vThenH = append(vThenH, verticalMoves...)
+				vThenH = append(vThenH, horizontalMoves...)
+				verticalFirstMoveset = generateMoveset(vThenH, verticalFirstController)
+			}
+
+			// set moveset accordingly
+			if horizontalHitsGap {
+				moveset += verticalFirstMoveset
+				robot.controller = verticalFirstController
+			} else if verticalHitsGap {
+				moveset += horizontalFirstMoveset
+				robot.controller = horizontalFirstController
+			} else {
+				if len(horizontalFirstMoveset) <= len(verticalFirstMoveset) {
+					moveset += horizontalFirstMoveset
+					robot.controller = horizontalFirstController
+				} else {
+					moveset += verticalFirstMoveset
+					robot.controller = verticalFirstController
+				}
+			}
+
+		} else if xDiff != 0 {
+			// just go horizontal
+			moveset += generateMoveset(horizontalMoves, robot.controller)
+		} else if yDiff != 0 {
+			// just go vertical
+			moveset += generateMoveset(verticalMoves, robot.controller)
+		}
+
+		moveset += generateMoveset([]Pos{A_DIR}, robot.controller)
+		robot.pos = move
+	}
+
+	return moveset
+}
+
+func getHumanInput(moves []Pos) string {
+	var inputSequence string
+	for _, move := range moves {
+		switch move {
+		case UP:
+			inputSequence += "^"
+		case DOWN:
+			inputSequence += "v"
+		case LEFT:
+			inputSequence += "<"
+		case RIGHT:
+			inputSequence += ">"
+		case A_DIR:
+			inputSequence += "A"
+		}
+	}
+
+	return inputSequence
 }
 
 func getCoord(val rune) Pos {
